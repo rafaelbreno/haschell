dcb18b10c8fb953b00c02c8081b3087137a284e0
Rafael Breno <32229014+rafaelbreno@users.noreply.github.com>
2024-07-08 13:37:31 -0300

    Add Character

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,9 @@
 module Main (main) where
 
 import System.Environment (getArgs)
+import Parser (readExpr)
 
 main :: IO ()
 main = do 
-  args <- getArgs
-  let num1 = read (head args) :: Int
-  let num2 = read (args !! 1) :: Int
-  putStrLn "Name:"
-  name <- getLine
-  putStrLn ("Hello, " ++ name)
-  putStrLn (show num1 ++ " + " ++ show num2 ++ " = " ++ show (num1 + num2))
+  (expr:_) <- getArgs
+  putStrLn (readExpr expr)
---
+++ src/Parser.hs
@@ -0,0 +1,128 @@
+module Parser (
+    LispVal(..),
+    parseExpr,
+    readExpr
+) where
+
+import Text.ParserCombinators.Parsec as P hiding (spaces)
+import Data.Char (digitToInt)
+import Numeric (readOct, readHex)
+import Data.Functor as Functor
+
+data LispVal = Atom       String 
+             | Bool       Bool
+             | Character  Char
+             | DottedList [LispVal] LispVal -- This is called "improper list", stores a list of all elements but the last one, and the stores the last element separately.
+             | List       [LispVal]
+             | Number     Integer
+             | String     String
+
+-- Atom
+parseAtom :: P.Parser LispVal
+parseAtom = do 
+  first <- P.letter P.<|> symbol
+  rest  <- many (P.letter P.<|> P.digit P.<|> symbol)
+  let atom = first:rest
+  return $ case atom of 
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _    -> Atom atom
+
+-- Bool
+
+-- Character
+parseCharacter :: P.Parser LispVal
+parseCharacter = do 
+  _ <- P.try $ P.string "#\\"
+  value <- parseNamedChar <|> parseAnyChar
+  return $ Character value
+
+parseNamedChar :: P.Parser Char
+parseNamedChar = do 
+  P.choice (map P.try 
+    [ P.string "space" >> return ' '
+    , P.string "newline" >> return '\n'
+    ])
+
+parseAnyChar :: P.Parser Char 
+parseAnyChar = do 
+  x <- P.anyChar 
+  notFollowedByAlphaNum 
+  return x
+
+notFollowedByAlphaNum :: P.Parser () 
+notFollowedByAlphaNum = do 
+  nextChar <- P.lookAhead $ P.optionMaybe P.alphaNum 
+  case nextChar of 
+    Just _ -> P.unexpected "character"
+    Nothing -> return ()
+
+-- DottedList
+
+-- List
+
+-- Number
+
+parseNumber :: P.Parser LispVal
+parseNumber = do 
+  prefix <- P.optionMaybe (P.char '#')
+  case prefix of 
+    Nothing -> parseDecimal
+    Just _ -> parsePrefixedNumber
+
+parseDecimal :: P.Parser LispVal
+parseDecimal = do 
+  digits <- P.many1 P.digit
+  return (Number ( read digits ))
+
+parsePrefixedNumber :: P.Parser LispVal
+parsePrefixedNumber = do 
+  base <- P.oneOf "bodx"
+  digits <- P.many1 ( P.digit <|> P.oneOf "abcdefABDEF")
+  return $ Number (convertToInt base digits)
+
+convertToInt :: Char -> String -> Integer
+convertToInt 'b' = bin2dec
+convertToInt 'o' = fst . head . readOct
+convertToInt 'd' = read
+convertToInt 'x' = fst . head . readHex
+convertToInt _ = error "not supported"
+
+bin2dec :: String -> Integer
+bin2dec = foldl (\acc x -> acc * 2 + toInteger (digitToInt x)) 0
+
+-- String
+
+parseString :: P.Parser LispVal
+parseString = do 
+  _ <- P.char '"'
+  x <- many parseStringChar
+  _ <- P.char '"'
+  return $ String x
+
+parseStringChar :: P.Parser Char
+parseStringChar = do 
+  P.char '\\' *> (
+    P.char '"' Functor.$> '"' 
+    <|> P.char 'n' Functor.$> '\n'
+    <|> P.char 'r' Functor.$> '\r'
+    <|> P.char 't' Functor.$> '\t'
+    <|> P.char '\\' Functor.$> '\\'
+    ) 
+  <|> noneOf "\\\""
+
+-- 
+
+parseExpr :: P.Parser LispVal
+parseExpr = parseAtom 
+  <|> parseString
+  <|> parseNumber
+  <|> parseCharacter
+
+symbol :: P.Parser Char
+symbol = P.oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case P.parse parseExpr "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right _ -> "Found a value"
