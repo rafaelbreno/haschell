a955ad76fd6e75ef5ab5136c0ff47a35016d26b6
Rafael Breno <32229014+rafaelbreno@users.noreply.github.com>
2024-07-15 14:38:27 -0300

    Add a few primitives

--- src/Parser.hs
+++ src/Parser.hs
@@ -251,4 +251,60 @@ eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool _) = val
 eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
 eval _ = error "not implemented yet"
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [
+  ("+", numericBinop (+)),
+  ("-", numericBinop (-)),
+  ("+", numericBinop (+)),
+  ("/", numericBinop div),
+  ("mod", numericBinop mod),
+  ("quotient", numericBinop quot),
+  ("remainder", numericBinop rem),
+  ("string?", isString),
+  ("number?", isNumber),
+  ("boolean?", isBool),
+  ("char?", isCharacter),
+  ("list?", isList)
+  ]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+--unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in 
+                           --if null parsed 
+                              --then 0
+                              --else fst $ head parsed
+--unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
+
+
+
+
+
+isBool :: [LispVal] -> LispVal
+isBool [Bool _] = Bool True
+isBool _ = Bool False
+
+isString :: [LispVal] -> LispVal
+isString [String _] = Bool True
+isString _ = Bool False
+
+isNumber :: [LispVal] -> LispVal
+isNumber [Number _] = Bool True
+isNumber _ = Bool False
+
+isCharacter :: [LispVal] -> LispVal
+isCharacter [Character _] = Bool True
+isCharacter _ = Bool False
+
+isList :: [LispVal] -> LispVal
+isList [List _] = Bool True
+isList _ = Bool False
